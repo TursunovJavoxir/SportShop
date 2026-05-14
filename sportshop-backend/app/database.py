a7445516d493415cac5fcb4e295172560bf6f1cb
TursunovJavoxir <sfjava94@gmail.com>
2026-05-14 20:55:20 +0500

    Fix database for render

--- sportshop-backend/app/database.py
+++ sportshop-backend/app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-
-DATABASE_URL = "postgresql://postgres:1234@localhost/sportshop"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
@@ -16,14 +15,9 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
-# подключение к БД
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
